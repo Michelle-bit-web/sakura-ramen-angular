a048ebee033fb710c1ef6a21b3be0dc14db55cc1
Michelle <mpuschkarow@web.de>
2025-05-20 16:15:05 +0200

    routerLink added to footer-imprint-link to keep base href & router module imported to footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { MainContentComponent } from './main-content/main-content.component';
+// import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { ImprintComponent } from './imprint/imprint.component';
+// import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
     CommonModule,
-    MainContentComponent,
+    // MainContentComponent,
     FooterComponent,
-    ImprintComponent,
+    // ImprintComponent,
   ],
   standalone: true,  
   templateUrl: './app.component.html',
